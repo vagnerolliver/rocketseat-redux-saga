eaff649e0230f77e7291b94139b77799c424c24f
Vagner Oliveira <vagner.olliver@yahoo.com.br>
2022-02-04 10:38:17 -0300

    chore: setup basic store

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Provider } from 'react-redux';
+
+import api from "./services/api";
+import store from './store'
+import { Catalog } from './compontens/Catalog'
 
 function App() {
+  // const [catalog, setCatalog] = useState([]);
+
+  // useEffect(() => {
+  //   api.get('products').then(response => {
+  //     setCatalog(response.data);
+  //   })
+  // },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Catalog />
+    </Provider>
   );
 }
 
