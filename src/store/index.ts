baa8d57cbf614a421b24327da1ec51f068bbf84f
Vagner Oliveira <vagner.olliver@yahoo.com.br>
2022-02-15 08:48:14 -0300

    chore: config react-devtools-extension

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import { CartState } from './modules/cart/types';
 import rootReducer from './modules/rootReducer'
@@ -7,6 +8,9 @@ export interface State {
   cart: CartState
 }
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer, 
+  composeWithDevTools()
+)
 
 export default store
